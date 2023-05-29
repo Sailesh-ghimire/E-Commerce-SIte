01bb3c362e7fcdc2f753bc0d19ae1ccf3b50ca6c
Sailesh Ghimire <saileshghimire.sg@gmail.com>
2023-05-29 23:22:36 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      <h1 className='text-red-500'>This is css</h1>
+      <Hello></Hello>
+      </div>
+    </div>
+  );
+}
+
+export default App;
